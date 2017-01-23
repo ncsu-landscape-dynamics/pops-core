0e129694b796c58781e6f410d51e0679239a39a9
Anna Petrasova <kratochanna@gmail.com>
2017-01-23 14:07:45 -0500

    add separate and more powerful date class

--- Spore.h
+++ Spore.h
@@ -46,36 +46,4 @@ public:
                          double gamma = 0.0);
 };
 
-class Date
-{
-private:
-    int year;
-    int month;
-    int day;
-    int day_in_month[2][13] = {
-        {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-        {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
-    };
-
-public:
-    Date(int y, int m, int d):year(y), month(m), day(d)
-    {
-    }
-    bool compareDate(Date & endtime);
-    void increasedByWeek();
-    int getMonth() const
-    {
-        return month;
-    }
-    int getYear() const
-    {
-        return year;
-    }
-    int getDay() const
-    {
-        return day;
-    }
-};
-
-
 #endif
