99bed7d61ba4eb131e42d81e3883c7f4c75f2c64
Vaclav Petras <wenzeslaus@gmail.com>
2018-07-27 16:04:03 -0400

    test complete simulation class workflow

--- test_simulation.cpp
+++ test_simulation.cpp
@@ -42,9 +42,27 @@ using std::endl;
 
 int main(int argc, char *argv[])
 {
-    Raster<int> a(10, 10, 1, 1);
-    Simulation<Raster<int>, Raster<double>> simulation(42, a);
-
+    Raster<int> infected = {{5, 0}, {0, 0}};
+    Raster<int> infected_cohort = {{0, 0}, {0, 0}};
+    Raster<int> susceptible = {{5, 6}, {14, 15}};
+    Raster<int> all = {{5, 0}, {0, 0}};
+    Raster<double> temperature = {{5, 0}, {0, 0}};
+    std::vector<std::tuple<int, int>> escaping;
+    Rtype rtype = CAUCHY;
+    double lethal_temperature = -4.5;
+    double weather_coef_value = 5.8;
+    double spore_rate = 4.5;
+    double scale1 = 18.7;
+    Simulation<Raster<int>, Raster<double>> simulation(42, infected);
+    simulation.SporeRemove(infected, susceptible,
+                           temperature, lethal_temperature);
+    simulation.SporeGen(infected, 0, weather_coef_value, spore_rate);
+    simulation.SporeSpreadDisp_singleSpecies(susceptible, infected,
+                                             infected_cohort, all,
+                                             escaping, rtype, 0,
+                                             weather_coef_value, scale1);
+    cout << infected;
+    cout << escaping.size() << endl;
     return 0;
 }
 
