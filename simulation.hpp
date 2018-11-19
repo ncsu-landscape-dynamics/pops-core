8f75f98b139dcc0b8c951b08d2e0eaaa40cdadf7
ChrisJones687 <cjones1688@gmail.com>
2018-11-19 10:38:29 -0500

    fixed undeclared variables

--- simulation.hpp
+++ simulation.hpp
@@ -162,15 +162,15 @@ public:
                    IntegerRaster& mortality, std::vector<IntegerRaster>& mortality_tracker_vector)
     {
         if (current_year >= (first_mortality_year)) {
-            mortality_current_year = 0;
-            max_year_index = current_year - first_mortality_year;
+            int mortality_current_year = 0;
+            int max_year_index = current_year - first_mortality_year;
             
             for (unsigned year_index = 0; year_index <= max_year_index; year_index++) {
               IntegerRaster mortality_in_year_index = {};
                 for (int i = 0; i < height; i++) {
                     for (int j = 0; j < width; j++) {
-                         if (mortality_tracker[year_index](i, j) > 0) {
-                           mortality_in_year_index(i,j) = mortality_rate*mortality_tracker[year_index](i,j);
+                         if (mortality_tracker_vector[year_index](i, j) > 0) {
+                           mortality_in_year_index(i,j) = mortality_rate*mortality_tracker_vector[year_index](i,j);
                            mortality_tracker_vector[year_index](i,j) -= mortality_in_year_index(i,j);
                            mortality(i,j) += mortality_in_year_index(i,j);
                            mortality_current_year += mortality_in_year_index(i,j);
