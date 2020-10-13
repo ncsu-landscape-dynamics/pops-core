82ed849624c09a3cda9489079bd2c1726e7652ff
MargaretLawrimore <margaret.lawrimore@gmail.com>
2020-10-13 15:10:04 -0400

    Added code from Rpops - pops.cpp

--- include/pops/utils.hpp
+++ include/pops/utils.hpp
@@ -49,7 +49,7 @@ enum class Direction
 {
     N = 0,  //!< North
     NE = 45,  //!< Northeast
-    E = 90,  //!< NEast
+    E = 90,  //!< East
     SE = 135,  //!< Southeast
     S = 180,  //!< South
     SW = 225,  //!< Southwest
@@ -57,5 +57,68 @@ enum class Direction
     NW = 315,  //!< Northwest
     None  //!< No direction (non-directional)
 };
+
+template<int... Indices>
+struct indices
+{
+    using next = indices<Indices..., sizeof...(Indices)>;
+};
+
+template<int Size>
+struct build_indices
+{
+    using type = typename build_indices<Size - 1>::type::next;
+};
+
+template<>
+struct build_indices<0>
+{
+    using type = indices<>;
+};
+
+template<typename T>
+using Bare = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
+
+template<typename Tuple>
+constexpr typename build_indices<std::tuple_size<Bare<Tuple>>::value>::type
+make_indices()
+{
+    return {};
+}
+
+template<typename Tuple, int... Indices>
+std::array<
+    typename std::tuple_element<0, Bare<Tuple>>::type,
+    std::tuple_size<Bare<Tuple>>::value>
+to_array(Tuple&& tuple, indices<Indices...>)
+{
+    using std::get;
+    return {{get<Indices>(std::forward<Tuple>(tuple))...}};
+}
+
+template<typename Tuple>
+auto to_array(Tuple&& tuple)
+    -> decltype(to_array(std::declval<Tuple>(), make_indices<Tuple>()))
+{
+    return to_array(std::forward<Tuple>(tuple), make_indices<Tuple>());
+}
+
+std::string quarantine_enum_to_string(Direction type)
+{
+    switch (type) {
+    case Direction::N:
+        return "N";
+    case Direction::S:
+        return "S";
+    case Direction::E:
+        return "E";
+    case Direction::W:
+        return "W";
+    case Direction::None:
+        return "None";
+    default:
+        return "Invalid direction";
+    }
+}
 
 #endif  // POPS_UTILS_HPP
