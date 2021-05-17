3450abfe1c6c204fa71cb2ad410fccdee297d355
ChrisJones687 <cjones1688@gmail.com>
2021-05-17 07:21:37 -0400

    update movement test.

--- tests/test_movements.cpp
+++ tests/test_movements.cpp
@@ -43,94 +43,92 @@ using namespace pops;
 
 int test_move_all()
 {
-  Raster<int> infected = {{5, 0}, {0, 0}};
-  Raster<int> susceptible = {{10, 5}, {5, 3}};
-  Raster<int> total_exposed = {{0, 0}, {0, 0}};
-  Raster<int> resistant = {{0, 0}, {0, 0}};
-  auto total_hosts = infected + susceptible + total_exposed + resistant;
-  std::vector<Raster<int>> exposed = {{{0, 0}, {0, 0}}, {{0, 0}, {0, 0}}};
-  std::vector<Raster<int>> mortality_tracker = {{{3, 0}, {0, 0}}, {{2, 0}, {0, 0}}};
-  unsigned step = 1;
-  last_index = 0;
-  std::vector<std::vector<int>> movements = {{0, 0, 1, 1, 15}, {0, 1, 1, 0, 5}};
-  std::vector<unsigned> movement_schedule = {1, 1};
-  // set expected new values after movement
-  Raster<int> expected_infected = {{0, 0}, {0, 5}};
-  Raster<int> expected_susceptible = {{0, 5}, {5, 13}};
-  Raster<int> expected_total_exposed = {{0, 0}, {0, 0}};
-  Raster<int> expected_resistant = {{0, 0}, {0, 0}};
-  Raster<int> expected_total_hosts = {{0, 5}, {5, 18}};
-  std::vector<Raster<int>> expected_exposed = {{{0, 0}, {0, 0}}, {{0, 0}, {0, 0}}};
-  std::vector<Raster<int>> expected_mortality_tracker = {{{0, 0}, {0, 3}}, {{0, 0}, {0, 2}}};
-
+    Raster<int> infected = {{5, 0}, {0, 0}};
+    Raster<int> susceptible = {{10, 5}, {5, 3}};
+    Raster<int> total_exposed = {{0, 0}, {0, 0}};
+    Raster<int> resistant = {{0, 0}, {0, 0}};
+    auto total_hosts = infected + susceptible + total_exposed + resistant;
+    std::vector<Raster<int>> exposed = {{{0, 0}, {0, 0}}, {{0, 0}, {0, 0}}};
+    std::vector<Raster<int>> mortality_tracker = {{{3, 0}, {0, 0}}, {{2, 0}, {0, 0}}};
+    unsigned step = 1;
+    int last_index = 0;
+    std::vector<std::vector<int>> movements = {{0, 0, 1, 1, 15}, {0, 1, 1, 0, 5}};
+    std::vector<unsigned> movement_schedule = {1, 1};
+    // set expected new values after movement
+    Raster<int> expected_infected = {{0, 0}, {0, 5}};
+    Raster<int> expected_susceptible = {{0, 5}, {5, 13}};
+    Raster<int> expected_total_exposed = {{0, 0}, {0, 0}};
+    Raster<int> expected_resistant = {{0, 0}, {0, 0}};
+    Raster<int> expected_total_hosts = {{0, 5}, {5, 18}};
+    std::vector<Raster<int>> expected_exposed = {{{0, 0}, {0, 0}}, {{0, 0}, {0, 0}}};
+    std::vector<Raster<int>> expected_mortality_tracker = {{{0, 0}, {0, 3}}, {{0, 0}, {0, 2}}};
     std::vector<std::vector<int>> suitable_cells = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-  Simulation<Raster<int>, Raster<double>> simulation(
-      42, infected.rows(), infected.cols());
-  simulation.movement(
-    infected,
-    susceptible,
-    mortality_tracker,
-    exposed,
-    resistant,
-    total_hosts,
-    total_exposed,
-    step,
-    last_index,
-    movements,
-    movement_schedule);
-  if (infected != expected_infected) {
-    cout << "infected (actual, expected):\n"
-         << infected << "  !=\n"
-         << expected_infected << "\n";
-    return 1;
-  }
-  if (susceptible != expected_susceptible) {
-    cout << "susceptible (actual, expected):\n"
-         << susceptible << "  !=\n"
-         << expected_susceptible << "\n";
-    return 1;
-  }
-  if (total_exposed != expected_total_exposed) {
-    cout << "total_exposed (actual, expected):\n"
-         << total_exposed << "  !=\n"
-         << expected_total_exposed << "\n";
-    return 1;
-  }
-  if (resistant != expected_resistant) {
-    cout << "resistant (actual, expected):\n"
-         << resistant << "  !=\n"
-         << expected_resistant << "\n";
-    return 1;
-  }
-  if (total_hosts != expected_total_hosts) {
-    cout << "total_hosts (actual, expected):\n"
-         << total_hosts << "  !=\n"
-         << expected_total_hosts << "\n";
-    return 1;
-  }
-  if (exposeds != expected_exposeds) {
-    cout << "exposeds (actual, expected):\n"
-         << exposeds << "  !=\n"
-         << expected_exposeds << "\n";
-    return 1;
-  }
-  if (mortality_trackers != expected_mortality_trackers) {
-    cout << "mortality_trackers (actual, expected):\n"
-         << mortality_trackers << "  !=\n"
-         << expected_mortality_trackers << "\n";
-    return 1;
-  }
-  return 0;
+    Simulation<Raster<int>, Raster<double>> simulation(
+        42, infected.rows(), infected.cols());
+    simulation.movement(
+      infected,
+      susceptible,
+      mortality_tracker,
+      exposed,
+      resistant,
+      total_hosts,
+      total_exposed,
+      step,
+      last_index,
+      movements,
+      movement_schedule);
+    if (infected != expected_infected) {
+      cout << "infected (actual, expected):\n"
+          << infected << "  !=\n"
+          << expected_infected << "\n";
+      return 1;
+    }
+    if (susceptible != expected_susceptible) {
+      cout << "susceptible (actual, expected):\n"
+          << susceptible << "  !=\n"
+          << expected_susceptible << "\n";
+      return 1;
+    }
+    if (total_exposed != expected_total_exposed) {
+      cout << "total_exposed (actual, expected):\n"
+          << total_exposed << "  !=\n"
+          << expected_total_exposed << "\n";
+      return 1;
+    }
+    if (resistant != expected_resistant) {
+      cout << "resistant (actual, expected):\n"
+          << resistant << "  !=\n"
+          << expected_resistant << "\n";
+      return 1;
+    }
+    if (total_hosts != expected_total_hosts) {
+      cout << "total_hosts (actual, expected):\n"
+          << total_hosts << "  !=\n"
+          << expected_total_hosts << "\n";
+      return 1;
+    }
+    if (exposed != expected_exposed) {
+      cout << "exposeds (actual, expected):\n"
+          << exposed << "  !=\n"
+          << expected_exposed << "\n";
+      return 1;
+    }
+    if (mortality_tracker != expected_mortality_tracker) {
+      cout << "mortality_tracker (actual, expected):\n"
+          << mortality_tracker << "  !=\n"
+          << expected_mortality_tracker << "\n";
+      return 1;
+    }
+    return 0;
 }
 
 
 
 int main()
 {
-  int num_errors = 0;
-
-  num_errors += test_move_all();
-  std::cout << "Movement number of errors: " << num_errors << std::endl;
-  return num_errors;
+    int num_errors = 0;
+    num_errors += test_move_all();
+    std::cout << "Movement number of errors: " << num_errors << std::endl;
+    return num_errors;
 }
 #endif  // POPS_TEST
