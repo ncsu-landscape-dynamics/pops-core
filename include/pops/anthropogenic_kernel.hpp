9b009b31709add47b2d00993d6b0ce04e3240bff
Vaclav Petras <wenzeslaus@gmail.com>
2021-09-07 16:52:24 -0400

    Remove invalid doc

--- include/pops/anthropogenic_kernel.hpp
+++ include/pops/anthropogenic_kernel.hpp
@@ -13,36 +13,6 @@
  * http://www.gnu.org/copyleft/gpl.html
  */
 
-/*! \file kernel.hpp
- *
- * \brief Main entry point to dispersal kernel functionality
- *
- * This file contains convenient definitions or wrappers to be used
- * when using this library.
- *
- * ## Adding a new kernel
- *
- * To add new kernel, decide if it needs to be a separate class,
- * or if it is just a parameterization of the existing kernel.
- * Many kernels can be handled by the RadialDispersalKernel class.
- *
- * Generally, such class needs to provide contructor taking all required
- * parameters and a function call operator which takes a random number
- * generator object and returns a pair of coordinates as row and column.
- * The signature of the function template should be:
- *
- * ```
- * template<typename Generator>
- * std::tuple<int, int> operator() (Generator& generator)
- * ```
- *
- * Besides implementation in a class, enum for the different types
- * of kernels needs to be extented as well as function which transforms
- * strings into enum values, i.e., you need to add to the
- * `DispersalKernelType` enum and extend the kernel_type_from_string()
- * function in kernel_types.hpp.
- */
-
 #ifndef POPS_ANTHROPOGENIC_KERNEL_HPP
 #define POPS_ANTHROPOGENIC_KERNEL_HPP
 
