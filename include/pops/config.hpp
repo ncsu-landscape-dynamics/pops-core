4ba30507253a28e0077738abdf1bd95f2842cd04
Vaclav Petras <wenzeslaus@gmail.com>
2022-04-06 13:37:42 -0400

    Fix step type

--- include/pops/config.hpp
+++ include/pops/config.hpp
@@ -71,7 +71,7 @@ public:
     std::string anthro_kernel_type;
     double anthro_scale{0};
     std::string anthro_direction;
-    double network_step{false};
+    bool network_step{false};
     double network_min_distance{0};
     double network_max_distance{0};
     bool network_snap{false};  ///< Snap resulting location to closest node in network
