f2c12b490ecb43d43f71106452e190d12e1027a8
ChrisJones687 <cjones1688@gmail.com>
2022-05-04 16:41:47 -0400

    changed deterministic to dispersal_stochasticity

--- include/pops/config.hpp
+++ include/pops/config.hpp
@@ -46,7 +46,7 @@ public:
     bool generate_stochasticity{true};
     bool establishment_stochasticity{true};
     bool movement_stochasticity{true};
-    bool deterministic{false};
+    bool dispersal_stochasticity{false};
     double establishment_probability{0};
     // Temperature
     bool use_lethal_temperature{false};
