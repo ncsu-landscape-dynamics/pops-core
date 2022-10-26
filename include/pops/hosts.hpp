8248ec522dfc07b74f8d43572b63adc810b95dd2
Vaclav Petras <wenzeslaus@gmail.com>
2022-10-26 12:57:50 -0400

    Increase version, use C++17, fix syntax in hosts draft code

--- include/pops/hosts.hpp
+++ include/pops/hosts.hpp
@@ -1,27 +1,34 @@
+#include <vector>
+
+namespace pops {
+
+template<typename IntegerRaster>
 class Host
 {
-    Raster infected;
-    vector<Raster> exposed;
-    Raster susceptible;
-    vector<cell> suitable_cells;
-    vector<Raster>mortality_tracker_vector;
-    Raster died;
+    IntegerRaster infected;
+    std::vector<IntegerRaster> exposed;
+    IntegerRaster susceptible;
+    std::vector<cell> suitable_cells;
+    std::vector<IntegerRaster> mortality_tracker_vector;
+    IntegerRaster died;
 
     double establishment_probability_at(i, j)
     {
-        double probability = double(this->susceptible_at(row, col)) / environment.total_population_at(row, col);
+        double probability = double(this->susceptible_at(row, col))
+                             / environment.total_population_at(row, col);
         if (weather)
             probability *= weather_coefficient(i, j);
         return probability;
     }
     double total_host_at(i, j)
     {
-        return this->susceptible_at(row, col) + this->infected_at(row, col) + this->exposed_at(row, col);
+        return this->susceptible_at(row, col) + this->infected_at(row, col)
+               + this->exposed_at(row, col);
     }
     double exposed_at(i, j)
     {
-        sum = 0
-        for raster in exposed:
+        sum = 0;
+        for (const IntegerRaster& raster : exposed)
             sum += raster(i, j);
         return sum;
     }
@@ -43,7 +50,7 @@ class Host
             exposed.back()(i, j) += 1;
         }
     }
-    void step_forward() // aka infect
+    void step_forward()  // aka infect
     {
         if ("SEI") {
             infected = exposed.front();
@@ -53,32 +60,34 @@ class Host
             rotate_left_by_one(mortality_tracker_vector);
         }
     }
-}
+};
 
+template<typename IntegerRaster>
 class Environment
 {
-    vector<Host*> hosts;  // non-owning reference
-    Raster other_individuals;  // non hosts
+    std::vector<Host*> hosts;  // non-owning reference
+    IntegerRaster other_individuals;  // non hosts
+
     int total_population_at(i, j)
     {
-        sum = other_individuals(i, j);
-        for host in hosts {
-            sum += host.total_host_at(i,j);
-        }
+        auto sum = other_individuals(i, j);
+        for (const auto& host : hosts)
+            sum += host.total_host_at(i, j);
         return sum;
     }
-}
+};
 
+template<typename IntegerRaster>
 class HostPool
 {
-    vector<Host> hosts;
+    std::vector<Host> hosts;
     bool disperser_to(i, j)
     {
-        vector probabilities;
-        for host in hosts {
+        std::vector probabilities;
+        for (const auto& host : hosts) {
             probabilities.push_back(host.establishment_probability_at(i, j));
         }
-        index = pick_host_index_by_probability(hosts, probabilities);
+        auto index = pick_host_index_by_probability(hosts, probabilities);
         probability = probabilities[index];
         if (establishment_stochasticity_)
             tester = distribution_uniform(generator_);
@@ -87,18 +96,18 @@ class HostPool
         if (tester < probability) {
             // establish
             host[index].add_disperser_at(i, j);
-            return true
+            return true;
         }
         // not established
         return false;
-        
     }
-}
+};
 
+template<typename IntegerRaster>
 class PestPool
 {
-    Raster mobile_dispersers;
-    Raster immobile_dispersers;  // aka in soil storage
+    IntegerRaster mobile_dispersers;
+    IntegerRaster immobile_dispersers;  // aka in soil storage
 
     void immobile_dispersers_to(number, i, j)
     {
@@ -120,8 +129,9 @@ class PestPool
         dispersers -= dispersers_to_soil;
         return dispersers;
     }
-}
+};
 
+template<typename Hosts, typename Pests>
 class Spread
 {
     // Generate and kernel part of disperse could be the same,
@@ -131,16 +141,16 @@ class Spread
 
     void action(Hosts hosts, Pests pests)
     {
-        for i, j in hosts.suitable_cells {
+        for (const auto& [i, j] : hosts.suitable_cells) {
             // Generate
             dispersers = hosts.dispersers_from(i, j);
             // To soils
             dispersers = pests.add_dispersers_at(dispersers, i, j);
-            
+
             // Classic spread
-            for disperser in dispersers {
+            for (const auto& disperser : dispersers) {
                 i, j, kernel_name = kernel(disperser);
-                //if (i, j is outside) {
+                // if (i, j is outside) {
                 //    pests.add_free(i, j);
                 //}
                 pests.add_landed(i, j);
@@ -151,92 +161,107 @@ class Spread
             }
             // From soils
             dispersers = pests.immobile_dispersers_from(i, j);
-            for disperser in dispersers {
+            for (const auto& disperser : dispersers) {
                 established = hosts.disperser_to(i, j);
                 if (established)
                     pests.add_established(i, j, i, j, "soil");
             }
         }
-
     }
-}
+};
 
-
+template<typename Hosts>
 class SurvivalRate
 {
     void action(Hosts hosts)
     {
-        for i, j in hosts.suitable_cells {
+        for (const auto& [i, j] : hosts.suitable_cells) {
             count = hosts.infected_at(i, j) * survival_rate(i, j);
             hosts.remove_infection_at(i, j, count);
         }
     }
-}
+};
 
+template<typename Hosts>
 class RemoveByTemperature
 {
     void action(Hosts hosts)
     {
-        for i, j in hosts.suitable_cells {
+        for (const auto& [i, j] : hosts.suitable_cells) {
             if (environment.temperature_at(i, j) < lethal_temperature) {
                 count = hosts.infected_at(i, j);
                 hosts.remove_infection_at(i, j, count);
             }
         }
     }
-}
+};
 
+template<typename Hosts>
 class PesticideTreatment
 {
     void apply_treatment(Hosts hosts)
     {
-        for i, j in hosts.suitable_cells {
+        for (const auto& [i, j] : hosts.suitable_cells) {
             susceptible = treatment(i, j) * hosts.get_susceptible_at(i, j);
             infected = get_resistant(treatment(i, j), hosts.infected_at(i, j));
             vector<int> exposed_list;
-            for value in hosts.exposed_at(i, j) {
-                exposed_list.push_back(get_resistant(treatment(i, j), value));
-            }
-            hosts.add_resistant_at(i, j, infected, susceptible, exposed_list);  // This could be an apply function with lambda.
+            for
+                value in hosts.exposed_at(i, j)
+                {
+                    exposed_list.push_back(get_resistant(treatment(i, j), value));
+                }
+            hosts.add_resistant_at(
+                i,
+                j,
+                infected,
+                susceptible,
+                exposed_list);  // This could be an apply function with lambda.
         }
     }
     void void end_treatment_effect(Hosts hosts)
     {
-        for i, j in hosts.suitable_cells {
+        for (const auto& [i, j] : hosts.suitable_cells) {
             hosts.remove_all_resistant_at(i, j);
         }
     }
-}
+};
 
+template<typename Hosts>
 class Mortality
 {
     void action(Hosts host)
     {
         // Do all mortality in Hosts and Host and here just call the right method.
         // Even mortality rate is linked to the host. Some don't die at all.
-        for i, j in hosts.suitable_cells {
-            for host in hosts {
-                for item in hosts.mortality_at(i, j) {
-                    if (index == 0)
-                        mortality = item;
-                    else
-                        mortality = mortality_rate * item;
-                    list.push_back(mortality);
-                }
+        for (const auto& [i, j] : hosts.suitable_cells) {
+            for
+                host in hosts
+                {
+                for
+                    item in hosts.mortality_at(i, j)
+                    {
+                        if (index == 0)
+                            mortality = item;
+                        else
+                            mortality = mortality_rate * item;
+                        list.push_back(mortality);
+                    }
                 hosts.kill_at(i, j, mortality);
-            }
+                }
         }
     }
-}
+};
 
+template<typename Hosts>
 class MoveOverpopulatedPests
 {
     void action(Hosts hosts)
     {
         // generate_moves as from cell, to cell, count won't work because
         // 1) they need to be airborne first and
-        // 2) only count for leaving=infected->susceptible is not tracked (no exposed and resistant)
-        for i, j in hosts.suitable_cells {
+        // 2) only count for leaving=infected->susceptible is not tracked (no exposed
+        // and resistant)
+        for (const auto& [i, j] : hosts.suitable_cells) {
             infected = hosts.infected_at(i, j);
             if (infected / hosts.total_hosts_at(i, j) >= overpopulation_percentage) {
                 i, j = kernel(disperser);
@@ -244,19 +269,23 @@ class MoveOverpopulatedPests
                 moves.push_back(i, j);
             }
         }
-        for i, j in moves {
-            hosts.disperser_to(i, j);
-        }
+        for
+            i, j in moves
+            {
+                hosts.disperser_to(i, j);
+            }
     }
-}
+};
 
+/*
+template<typename Hosts>
 class Model
 {
     Hosts hosts;
-    hosts.set_infected(...)
-    ... // more rasters added here (or all are set in the ctor)
+    hosts
+        .set_infected(...)...  // more rasters added here (or all are set in the ctor)
 
-    remove_by_temperature.action(hosts);
+        remove_by_temperature.action(hosts);
     survival_rate.action(hosts);
 
     move_overpopulated_pests.action(hosts);
@@ -270,3 +299,5 @@ class Model
 
     hosts.step_forward();
 }
+*/
+}  // namespace pops
